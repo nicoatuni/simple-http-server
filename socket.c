8b532004e9b8d1affce40cfec396f42624481fb4
Nico Dinata <n.dinata@student.unimelb.edu.au>
2018-04-17 00:16:24 +1000

    Include another system network header file

--- socket.c
+++ socket.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 // Handles creating, listening, and accepting socket connection
